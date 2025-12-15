f017c97d4add70898680c1543c3b19274a86286a
Elad Abutbul <eladabutbul61@gmail.com>
2025-12-15 10:09:35 +0200

    winner is function is created

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,7 +8,42 @@ export default function Game() {
   const [isXNext, setIsXNext] = useState(true);
   const [timer, setTimer] = useState(10);
   const [stop, setStop] = useState(false);
-  
+
+  function calculateWinner(): "X" | "O" | null {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const a = lines[i][0];
+      const b = lines[i][1];
+      const c = lines[i][2];
+
+      if (
+        squares[a] !== null &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a] as "X" | "O";
+      }
+    }
+
+    return null;
+  }
+
+  const winner = calculateWinner();
+
+  useEffect(() => {
+    if (winner) setStop(true);
+  }, [winner]);
+
   function handleSquareClick(index: number) {
     if (stop) return;
     if (squares[index]) return;
@@ -18,6 +53,7 @@ export default function Game() {
     setIsXNext((prev) => !prev);
     setTimer(10);
   }
+
   function handleReset() {
     setIsXNext(true);
     setSquares(Array(9).fill(null));
@@ -26,18 +62,18 @@ export default function Game() {
   }
 
   useEffect(() => {
-    if (!stop) {
-      const timerId = setTimeout(() => {
-        if (timer == 0) {
-          setIsXNext((prev) => !prev);
-          setTimer(10);
-        } else {
-          setTimer((time) => time - 1);
-        }
-      }, 1000);
-      return () => clearTimeout(timerId);
-    }
-  }, [timer, stop]);
+    if (stop || winner) return;
+    const timerId = setTimeout(() => {
+      if (timer === 0) {
+        setIsXNext((prev) => !prev);
+        setTimer(10);
+      } else {
+        setTimer((prev) => prev - 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  }, [timer, stop, winner]);
 
   function handleStop() {
     setStop((stop) => !stop);
@@ -45,13 +81,22 @@ export default function Game() {
   return (
     <div className="game-container">
       <h1>Tic Tac Toe</h1>
-      <h2>
-        time for player {isXNext ? "X" : "O"}- {timer} sec
-      </h2>
+      {winner && <h2>Winner: {winner}</h2>}
+      {winner ? (
+        ""
+      ) : (
+        <h2>
+          time for player {isXNext ? "X" : "O"}- {timer} sec
+        </h2>
+      )}
       <Board squares={squares} onSquareClick={handleSquareClick} />
-      <p>Next Player: {isXNext ? "X" : "O"}</p>
+      {winner ? "" : <p>Next Player: {isXNext ? "X" : "O"}</p>}
       <button onClick={handleReset}>reset</button>
-      <button onClick={handleStop}>{stop ? "resume" : "stop"}</button>
+      {winner ? (
+        ""
+      ) : (
+        <button onClick={handleStop}>{stop ? "resume" : "stop"}</button>
+      )}
     </div>
   );
 }
